8c09e71ba083dda6352c955e6fd0bb3aea359562
samaykhushi <110050998+samaykhushi@users.noreply.github.com>
2023-10-15 07:49:43 +0000

    File-Upload

---
+++ wowbhai.py
@@ -0,0 +1,15 @@
+
+
+import random
+import string
+
+
+
+s = string.ascii_lowercase
+
+
+lists = []
+lists.extend(list(s))
+random.shuffle(lists)
+print("".join(lists[0:5]))
+# helol mu nam eis differen t from oither rapper inb the same way 
